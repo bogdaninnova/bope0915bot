f0b9c58a67dd28cccc6298fd0179b567f76b15f5
bope0915 <bogdan.innova@gmail.com>
2016-05-17 12:58:50 +0300

    Bot shows exchange rates

--- src/bot.py
+++ src/bot.py
@@ -5,46 +5,95 @@ from telebot import types
 import src.translation
 import time
 
-current_word = ''
 
 conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='191993'")
 
 
 def listener(messages):
-    global current_word
     for m in messages:
         if m.content_type == 'text':
-            if m.text.upper() == '/TEST':
-                pass
-            nextWord = True
-            if current_word != '':
+            next_word = False
+            user_word = True
+            if m.text.upper() == 'TEST':
+                user_word = False
+                cur = conn.cursor()
+                cur.execute('select word from mylist where userid = %s and rating != 100 ORDER BY RANDOM() LIMIT 1',
+                            (m.chat.id,))
+                row = cur.fetchone()
+                if row is None:
+                    bot.send_message(m.chat.id, "You haven't unknown words in your list.")
+                else:
+                    translation = src.translation.get_translation(row[0])
+                    bot.send_message(m.chat.id, "Word " + row[0] + " is " + translation)
+            if m.text.upper() == 'HELP':
+                user_word = False
+                cur = conn.cursor()
+                cur.execute('select count(1) from mylist where userid = %s', (m.chat.id,))
+                bot.send_message(m.chat.id, 'You have ' + str(cur.fetchone()[0]) + ' words in your list.')
+            cur = conn.cursor()
+            cur.execute('select current_word from currentwords where userid = %s', (m.chat.id,))
+            row = cur.fetchone()
+            if row is not None:
+                current_word = row[0]
+                if m.text.upper() == 'CANCEL':
+                    user_word = False
+                    markup = types.ReplyKeyboardMarkup()
+                    markup.row('TRANSLATE - ' + current_word)
+                    markup.row('LEARN', 'SKIP')
+                    markup.row('HELP', 'TEST')
+                    bot.send_message(m.chat.id, current_word, reply_markup=markup)
                 if m.text.upper() == 'LEARN':
-                    conn.cursor().execute("INSERT INTO mylist(userid, word, rating)"
-                                          " values(%s, %s, %s)",
+                    conn.cursor().execute("INSERT INTO mylist(userid, word, rating) values(%s, %s, %s)",
                                           (m.chat.id, current_word, 0))
                     conn.commit()
+                    next_word = True
+                    user_word = False
                 elif m.text.upper() == 'SKIP':
-                    conn.cursor().execute("INSERT INTO mylist(userid, word, rating)"
-                                          " values(%s, %s, %s)",
+                    conn.cursor().execute("INSERT INTO mylist(userid, word, rating) values(%s, %s, %s)",
                                           (m.chat.id, current_word, 100))
                     conn.commit()
-                elif m.text.upper() == 'TRANSLATE':
+                    next_word = True
+                    user_word = False
+                elif m.text.upper()[:12] == 'TRANSLATE - ':
+                    user_word = False
                     markup = types.ReplyKeyboardMarkup()
                     markup.row('LEARN', 'SKIP')
-                    translation = src.translation.get_translation(current_word)
-                    bot.send_message(m.chat.id, current_word + ' - ' + translation,
-                                     reply_markup=markup)
-                    nextWord = False
-            if nextWord:
+                    markup.row('HELP', 'TEST')
+                    translation = src.translation.get_translation(m.text[12:])
+                    bot.send_message(m.chat.id, m.text[12:] + ' - ' + translation, reply_markup=markup)
+            if next_word:
                 cur = conn.cursor()
                 cur.execute('select word from wordslist where word not in'
                             ' (select word from mylist where userid = %s) LIMIT 1', (m.chat.id,))
                 current_word = cur.fetchone()[0]
-
                 markup = types.ReplyKeyboardMarkup()
-                markup.row('LEARN', 'TRANSLATE', 'SKIP')
+                markup.row('TRANSLATE - ' + current_word)
+                markup.row('LEARN', 'SKIP')
+                markup.row('HELP', 'TEST')
                 bot.send_message(m.chat.id, current_word, reply_markup=markup)
+                conn.cursor().execute("UPDATE currentwords SET current_word = %s WHERE userID = %s",
+                                      (current_word, m.chat.id))
+                conn.cursor().execute("INSERT INTO currentwords(userid, current_word)"
+                                      " values(%s, %s) ON CONFLICT (userid) DO NOTHING", (m.chat.id, current_word))
+                conn.commit()
 
+            if user_word:
+                cur = conn.cursor()
+                cur.execute('select 1 from mylist where userid = %s and upper(word) = %s',
+                                      (m.chat.id, m.text.upper()))
+                if cur.fetchone() is not None:
+                    markup = types.ReplyKeyboardMarkup()
+                    markup.row('TRANSLATE - ' + m.text)
+                    markup.row('DROP "' + m.text + '"')
+                    markup.row('KNOW', "DON'T KNOW")
+                    bot.send_message(m.chat.id, 'Word "' + m.text + '" is in your vocabulary', reply_markup=markup)
+                else:
+                    markup = types.ReplyKeyboardMarkup()
+                    markup.row('TRANSLATE - ' + m.text)
+                    markup.row('ADD "' + m.text + '"')
+                    markup.row('CANCEL')
+                    bot.send_message(m.chat.id, 'Do you want to add word "' + m.text + '" to your vocabulary?',
+                                     reply_markup=markup)
 
 if __name__ == '__main__':
     bot = telebot.TeleBot(src.config.botToken)
